c47cc90dc7c389cb026c6edb80a71ab39d8f6da5
MartinaRosalesF <martina.m.rosales@gmail.com>
2021-11-08 19:58:06 -0300

    confirmar

---
+++ modulos145.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  Historial precargado de partidas de tateti
+ * @return array 
+ */
+function cargarJuegos ()
+{
+    $historialJuegos [0] = ["jugadorX" => "TOMI", "jugadorO" => "JUAN", "puntosX" => 5, "puntosO" => 0];
+    $historialJuegos [1] = ["jugadorX" => "JUAN", "jugadorO" => "ANTO", "puntosX" => 1, "puntosO" => 1];
+    $historialJuegos [2] = ["jugadorX" => "GONZI", "jugadorO" => "DEDU", "puntosX" => 0, "puntosO" => 4];
+    $historialJuegos [3] = ["jugadorX" => "GALA", "jugadorO" => "NICO", "puntosX" => 0, "puntosO" => 5];
+    $historialJuegos [4] = ["jugadorX" => "NICO", "jugadorO" => "TOMI", "puntosX" => 1, "puntosO" => 1];
+    $historialJuegos [5] = ["jugadorX" => "MARI", "jugadorO" => "ALEJO", "puntosX" => 3, "puntosO" => 0];
+    $historialJuegos [6] = ["jugadorX" => "ALEJO", "jugadorO" => "MARI", "puntosX" => 1, "puntosO" => 1];
+    $historialJuegos [7] = ["jugadorX" => "FINN", "jugadorO" => "FACU", "puntosX" => 4, "puntosO" => 0];
+    $historialJuegos [8] = ["jugadorX" => "FACU", "jugadorO" => "GONZI", "puntosX" => 1, "puntosO" => 1];
+    $historialJuegos [9] = ["jugadorX" => "ANTO", "jugadorO" => "GALA", "puntosX" => 2, "puntosO" => 0];
+    return $historialJuegos;
+}
