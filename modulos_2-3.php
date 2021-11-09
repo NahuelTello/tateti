da4a5493e47d13cb1ab0f99da7eb1dd988a933c8
NahuelTello <matias.tello@est.fi.uncoma.edu.ar>
2021-11-09 15:57:29 -0300

    modificado

--- modulos_2-3.php
+++ modulos_2-3.php
@@ -1,1 +1,35 @@
 <?php
+/**
+ * Muestra las opciones del menú de pantalla, donde se le solicita al usuario
+ * una opcion valida y retorne el numero de la opcion. La ultima opcion del 
+ * menu debe ser "Salir"
+ * 
+ * @param int $opcion
+ * @return int
+ */
+
+function seleccionarOpcion($opcion){
+    switch ($opcion) {
+        case 'value':
+            # code...
+            break;
+        case 'value':
+            # code...
+            break;
+        default:
+            # code...
+            break;
+    }
+}
+
+/**
+ * Solicita al usuario un numero entre un rango de valores. Si el numero ingresado
+ * no es valido, la funcion se encarga de volver a pedirlo. Retorna un numero valido.
+ * 
+ * @param int $numeroValidar
+ * @return int 
+ */
+
+function validarOpcion(){
+    
+}
