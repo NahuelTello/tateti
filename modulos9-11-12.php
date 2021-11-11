802a1499c64c61bf29871b4b252a8b681650b18e
milicarcamo <milagros.carcamo@est.fi.uncoma.edu.ar>
2021-11-11 19:38:23 -0300

    confirmado

--- modulos9-11-12.php
+++ modulos9-11-12.php
@@ -23,7 +23,7 @@ function seleccionarSimbolo () {
 $simboloFinal = seleccionarSimbolo() ; //Lo invoque para probar si funcionaba, Borrar despues
 
 
-
+include_once("modulos145.php"); //Inclui el modulo que hizo Marti, asi lo puedo invocar mas abajo para probarlo. Cuando lo pasemos al programaApellidos lo borramos ya q no va a hacer falta
 /**
  * Funcion 10 --> "Explicacion 3"
  * Dada una colección de juegos y un símbolo (X o O) retorna la cantidad de juegos ganados por el símbolo ingresado por parámetro.
@@ -35,18 +35,26 @@ function victoriasDeSimbolos ($coleccionJuegos, $simbolo) {
     //INT $juegosGanadosSimbolo, $i, $cantElementosArray
     $cantElementosArray = count($coleccionJuegos) ;
     $juegosGanadosSimbolo = 0 ; // Cuenta de la cant de juegos ganados por el simbolo ingresado
-    for ($i=0; $i < $cantElementosArray ; $i++) { 
-        
+    for ($i=0; $i < $cantElementosArray ; $i++) {
+        if (($simbolo == "X") && ($coleccionJuegos[$i] ["puntosX"] > $coleccionJuegos[$i] ["puntosO"])){
+            $juegosGanadosSimbolo = $juegosGanadosSimbolo + 1;
+        }elseif (($simbolo == "O") && ($coleccionJuegos[$i] ["puntosX"] < $coleccionJuegos[$i] ["puntosO"])) {
+                $juegosGanadosSimbolo = $juegosGanadosSimbolo + 1;
+        }    
     }
+    return $juegosGanadosSimbolo ;
 }
-
+//Estas 3 lineas de abajo las hice para probar el modulo, ¡¡¡BORRAR DESPUES!!!
+$coleccJuegos = cargarJuegos() ; //Aca invoco al modulo que hizo Marti, para ver el historial de juegos
+$juegosGanados = victoriasDeSimbolos($coleccJuegos, $simboloFinal ) ;
+echo "La cant de victorias del simbolo ". $simboloFinal . " es: " . $juegosGanados ;
 
 /**
- * Funcion 11 --> "Explicacion 3"
+ * Función 11 -> "Explicacion 3"
  * Implementar una función sin retorno que, dada una colección de juegos, 
  * muestre la colección de juegos ordenado por el nombre del jugador cuyo símbolo es O.
- * @param array $coleccionDeJuegos
+ * @param array $ coleccionDeJuegos
  */
-function ordenaJugadoresO ($coleccionDeJuegos) {
+function ordenaJugadoresO ( $coleccionDeJuegos) {
     
 }
