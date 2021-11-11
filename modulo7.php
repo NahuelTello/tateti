c279aa464c78967a65e87eff15b1738b9256cf03
NahuelTello <matias.tello@est.fi.uncoma.edu.ar>
2021-11-11 20:32:12 -0300

    cambios

--- modulo7.php
+++ modulo7.php
@@ -1,21 +1,38 @@
 <?php
-
+include_once("modulos145.php");
 /**
  * Dada una colección de juegos y el nombre de un jugador, retorna el indice del primer juego ganado por dicho jugador.
  * Si el jugador no gano ningun juego, la funcion debe retornar el valor -1.
  * 
- * @param array $coleccionJuegos
- * @param String $nombreJugador
- * @return
+ * @param array $coleccionJuegosBuscados
+ * @param String $nombreJugadorBuscado
+ * @return int
  */
 
-function primerVictoria($coleccionJuegos,$nombreJugador){
+function primerVictoria($coleccionJuegosBuscados,$nombreJugadorBuscado){
+    /* int $i, $primerJuegoGanado, $longArreglo*/
+    /* boolean $juegoEncontrado */
 
-    $primerJuegoGanado = 0;
+    $primerJuegoGanado = -1;
     $juegoEncontrado = true;
-    while (!$juegoEncontrado) {
-        
+    $longArreglo = count($coleccionJuegosBuscados);
+    $i=0;
+    while ($juegoEncontrado && ($i < $longArreglo)) {
+        if ($coleccionJuegosBuscados[$i]["jugadorX"]==$nombreJugadorBuscado) {
+            if ($coleccionJuegosBuscados[$i]["puntosX"] > $coleccionJuegosBuscados[$i]["puntosO"]) {
+                $primerJuegoGanado = $i;
+                $juegoEncontrado = false;
+            }
+        } elseif ($coleccionJuegosBuscados[$i]["jugadorO"] == $nombreJugadorBuscado) {
+            if ($coleccionJuegosBuscados[$i]["puntosO"] > $coleccionJuegosBuscados[$i]["puntosX"]) {
+                $primerJuegoGanado = $i;
+                $juegoEncontrado = false;
+            }
+        } /* else {
+            $primerJuegoGanado = -1;
+        } */
+        $i++;
     }
 
-    /* return $indiceJuegoGanado; */
+    return $primerJuegoGanado;
 }
