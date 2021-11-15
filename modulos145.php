7b019d73484e455d8ce3cf7e0ae76a5fbdfa3e78
MartinaRosalesF <martina.m.rosales@gmail.com>
2021-11-15 17:30:33 -0300

    confirmado

--- modulos145.php
+++ modulos145.php
@@ -1,20 +1,62 @@
 <?php
+include_once ("tateti.php");
 /**
  *  Historial precargado de partidas de tateti
  * @return array 
  */
 function cargarJuegos ()
 {
-    $historialJuegos [0] = ["jugadorX" => "TOMI", "jugadorO" => "JUAN", "puntosX" => 5, "puntosO" => 0];
-    $historialJuegos [1] = ["jugadorX" => "JUAN", "jugadorO" => "ANTO", "puntosX" => 1, "puntosO" => 1];
-    $historialJuegos [2] = ["jugadorX" => "GONZI", "jugadorO" => "DEDU", "puntosX" => 0, "puntosO" => 4];
-    $historialJuegos [3] = ["jugadorX" => "GALA", "jugadorO" => "NICO", "puntosX" => 0, "puntosO" => 5];
-    $historialJuegos [4] = ["jugadorX" => "NICO", "jugadorO" => "TOMI", "puntosX" => 1, "puntosO" => 1];
-    $historialJuegos [5] = ["jugadorX" => "MARI", "jugadorO" => "ALEJO", "puntosX" => 3, "puntosO" => 0];
-    $historialJuegos [6] = ["jugadorX" => "ALEJO", "jugadorO" => "MARI", "puntosX" => 1, "puntosO" => 1];
-    $historialJuegos [7] = ["jugadorX" => "FINN", "jugadorO" => "FACU", "puntosX" => 4, "puntosO" => 0];
-    $historialJuegos [8] = ["jugadorX" => "FACU", "jugadorO" => "GONZI", "puntosX" => 1, "puntosO" => 1];
-    $historialJuegos [9] = ["jugadorX" => "ANTO", "jugadorO" => "GALA", "puntosX" => 2, "puntosO" => 0];
-    return $historialJuegos;
+    $juegosPrecargados [0] = ["jugadorX" => "TOMI", "jugadorO" => "JUAN", "puntosX" => 5, "puntosO" => 0];
+    $juegosPrecargados [1] = ["jugadorX" => "JUAN", "jugadorO" => "ANTO", "puntosX" => 1, "puntosO" => 1];
+    $juegosPrecargados [2] = ["jugadorX" => "GONZI", "jugadorO" => "DEDU", "puntosX" => 0, "puntosO" => 4];
+    $juegosPrecargados [3] = ["jugadorX" => "GALA", "jugadorO" => "NICO", "puntosX" => 0, "puntosO" => 5];
+    $juegosPrecargados [4] = ["jugadorX" => "NICO", "jugadorO" => "TOMI", "puntosX" => 1, "puntosO" => 1];
+    $juegosPrecargados [5] = ["jugadorX" => "MARI", "jugadorO" => "ALEJO", "puntosX" => 3, "puntosO" => 0];
+    $juegosPrecargados [6] = ["jugadorX" => "ALEJO", "jugadorO" => "MARI", "puntosX" => 1, "puntosO" => 1];
+    $juegosPrecargados [7] = ["jugadorX" => "FINN", "jugadorO" => "FACU", "puntosX" => 4, "puntosO" => 0];
+    $juegosPrecargados [8] = ["jugadorX" => "FACU", "jugadorO" => "GONZI", "puntosX" => 1, "puntosO" => 1];
+    $juegosPrecargados [9] = ["jugadorX" => "ANTO", "jugadorO" => "GALA", "puntosX" => 2, "puntosO" => 0];
+    return $juegosPrecargados;
 }
-echo "esto funca?";
+
+/**
+ * Agrega la última partida al historial de juegos de tateti
+ * @param array $juego
+ * @param array $historialJuegos
+ * @return array
+ */
+
+ 
+function agregarJuego ($historialJuegos, $juego)
+//string $jugadorX, $jugadorO
+//int $puntosX, $puntosO, $nroIndice
+{
+   array_push($historialJuegos, $juego);
+   return $historialJuegos;
+}
+
+
+
+/**
+ * muestra los datos de determinada partida de tateti.
+ * @param int $nroPartida
+ */
+function mostrarJuego ($historialJuegos)
+{
+    echo "Ingrese el número de partida que desea ver";
+    $nroPartida = trim(fgets(STDIN));
+    if ($historialJuegos[$nroPartida]["puntosX"] == $historialJuegos[$nroPartida]["puntosO"]){
+        $resultado = "empate";
+    }else if ($historialJuegos[$nroPartida]["puntosX"] < $historialJuegos[$nroPartida]["puntosO"]){
+        $resultado = "ganó O";
+    }else{
+        $resultado = "ganó X";
+    }
+    echo "****************************************";
+    echo "Juego TATETI: ". $nroPartida." (". $resultado. ")";
+    echo "Jugador X: ". $historialJuegos[$nroPartida]["jugadorX"]. " obtuvo ". $historialJuegos[$nroPartida]["puntosX"]. " puntos.";
+    echo "jugador O: ". $historialJuegos[$nroPartida]["jugadorO"]. " obtuvo ". $historialJuegos[$nroPartida]["puntosO"]. " puntos.";
+    echo "****************************************";
+}
+
+
