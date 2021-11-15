d981903926eb56b4e70986882b300696713b4fbd
NahuelTello <matias.tello@est.fi.uncoma.edu.ar>
2021-11-15 18:07:20 -0300

    cambios

--- programaCarcamoTelloRosales.php
+++ programaCarcamoTelloRosales.php
@@ -15,7 +15,7 @@ include_once("tateti.php");
 /**************************************/
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
-
+/*********** PARTE NAHUEL **********/
 /**
  * funcion 2 <-- Explicacion 3
  * Muestra las opciones del menú de pantalla, donde se le solicita al usuario
@@ -55,6 +55,60 @@ function validarOpcion ($numMin, $numMax) {
 
 
 
+/**
+ *  Historial precargado de partidas de tateti
+ * @return array 
+ */
+function cargarJuegos (){
+    $juegosPrecargados [0] = ["jugadorX" => "TOMI", "jugadorO" => "JUAN", "puntosX" => 5, "puntosO" => 0];
+    $juegosPrecargados [1] = ["jugadorX" => "JUAN", "jugadorO" => "ANTO", "puntosX" => 1, "puntosO" => 1];
+    $juegosPrecargados [2] = ["jugadorX" => "GONZI", "jugadorO" => "DEDU", "puntosX" => 0, "puntosO" => 4];
+    $juegosPrecargados [3] = ["jugadorX" => "GALA", "jugadorO" => "NICO", "puntosX" => 0, "puntosO" => 5];
+    $juegosPrecargados [4] = ["jugadorX" => "NICO", "jugadorO" => "TOMI", "puntosX" => 1, "puntosO" => 1];
+    $juegosPrecargados [5] = ["jugadorX" => "MARI", "jugadorO" => "ALEJO", "puntosX" => 3, "puntosO" => 0];
+    $juegosPrecargados [6] = ["jugadorX" => "ALEJO", "jugadorO" => "MARI", "puntosX" => 1, "puntosO" => 1];
+    $juegosPrecargados [7] = ["jugadorX" => "FINN", "jugadorO" => "FACU", "puntosX" => 4, "puntosO" => 0];
+    $juegosPrecargados [8] = ["jugadorX" => "FACU", "jugadorO" => "GONZI", "puntosX" => 1, "puntosO" => 1];
+    $juegosPrecargados [9] = ["jugadorX" => "ANTO", "jugadorO" => "GALA", "puntosX" => 2, "puntosO" => 0];
+    return $juegosPrecargados;
+}
+
+/**
+ * Agrega la última partida al historial de juegos de tateti
+ * @param array $juego
+ * @param array $historialJuegos
+ * @return array
+ */
+
+function agregarJuego ($historialJuegos, $juego){
+    //string $jugadorX, $jugadorO
+    //int $puntosX, $puntosO, $nroIndice
+    array_push($historialJuegos, $juego);
+    return $historialJuegos;
+}
+
+
+
+/**
+ * muestra los datos de determinada partida de tateti.
+ * @param int $nroPartida
+ */
+function mostrarJuego ($historialJuegos){
+    echo "Ingrese el número de partida que desea ver";
+    $nroPartida = trim(fgets(STDIN));
+    if ($historialJuegos[$nroPartida]["puntosX"] == $historialJuegos[$nroPartida]["puntosO"]){
+        $resultado = "empate";
+    }else if ($historialJuegos[$nroPartida]["puntosX"] < $historialJuegos[$nroPartida]["puntosO"]){
+        $resultado = "ganó O";
+    }else{
+        $resultado = "ganó X";
+    }
+    echo "****************************************";
+    echo "Juego TATETI: ". $nroPartida." (". $resultado. ")";
+    echo "Jugador X: ". $historialJuegos[$nroPartida]["jugadorX"]. " obtuvo ". $historialJuegos[$nroPartida]["puntosX"]. " puntos.";
+    echo "jugador O: ". $historialJuegos[$nroPartida]["jugadorO"]. " obtuvo ". $historialJuegos[$nroPartida]["puntosO"]. " puntos.";
+    echo "****************************************";
+}
 
 
 
@@ -88,7 +142,7 @@ do {
             break;
         case 2:
             echo "2) Mostrar un juego \n";
-            cargarJuegos();
+            mostrarJuego();
             break;
         case 3:
             echo "3) Mostrar el primer juego ganador \n";
@@ -112,7 +166,7 @@ do {
             echo "5) Mostrar resumen de jugador \n";
             echo "Ingrese el nombre del jugador: ";
             $nombreJugador = trim(fgets(STDIN));
-            $resumenJuego = resumenJugador($juegosTotales, $nombreJugador);
+            $resumenJuego = resumenJugador($juegosTotales, $nombreJugador); //juegosTotales variable no definida, me hace falta sacar esa info
              echo " *********************************** \n";
                 echo " Jugador: " . $resumenJuego["nameJugador"];
                 echo " Gano: ".$resumenJuego["cantGanadas"]." juegos\n";
@@ -129,4 +183,4 @@ do {
     }
     $numeroMenu = seleccionarOpciones ();
 
-} while ($opcion >= 1 && $opcion <= 7);
+} while ($opcion > 1 && $opcion < 7);
