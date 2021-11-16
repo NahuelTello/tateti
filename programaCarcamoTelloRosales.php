4ff6717b8ceb61629bf25b8d6de886e18d9fc99c
milicarcamo <milagros.carcamo@est.fi.uncoma.edu.ar>
2021-11-16 18:41:46 -0300

    cambio

--- programaCarcamoTelloRosales.php
+++ programaCarcamoTelloRosales.php
@@ -346,7 +346,7 @@ do {
             break;
         case 3:
             echo "Ingrese el nombre del jugador que desea buscar: ";
-            $jugador = trim(fgets(STDIN));
+            $jugador = strtoupper(trim(fgets(STDIN)));
             $res = buscaPrimerVictoria($arrayGames,$jugador);
             echo $res."\n";
             if ($res > -1) {
